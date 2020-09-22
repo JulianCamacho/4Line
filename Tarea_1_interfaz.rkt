ee4817b954c5d67a3499cf4497239a2faed2d4eb
José Fabián Mendoza Mata <49073441+JoseMendozaMata@users.noreply.github.com>
2020-09-22 14:31:40 -0600

    [Added] Se añade interfaz grafica funcional

--- Tarea_1_interfaz.rkt
+++ Tarea_1_interfaz.rkt
@@ -1,9 +1,11 @@
 #lang racket/base
+
 (require racket/gui)
 (require racket/include)
 
+(require (prefix-in htdp: 2htdp/image))
+
 (require "Logica.rkt")
-(require "foo.rkt")
 ;(include "Logica.rkt")
 ;(include "foo.rkt")
 
@@ -18,19 +20,83 @@
 ; Hace un botón para tirar una ficha en el juego
 (define (crear_boton col)
 
-  (new button% [parent panel]
+  (new button% [parent panel_botones]
              [label (number->string col)]
              ; Callback procedure for a button click:
              [callback (lambda (button event)
                          (writeln (play 1 col (car matrix) ) )
                          (asignar_matriz (play 1 col (car matrix)))
+                         (refrescar_tablero)
+                         (crear_matriz_interfaz (car matrix))
                          )]
                         
   )
   
 )
 
+;
 
+; Hace el espacio dependiendo del numero que toque
+
+(define (crear_espacio num panelx)
+
+  (if(= num 0)
+     (new message% [parent panelx]
+            [label (read-bitmap "EspacioVacio.png" )])
+     
+     #f
+  )
+  (if(= num 1)
+     (new message% [parent panelx]
+            [label (read-bitmap "EspacioJugador.png" )])
+     
+     #f
+  )
+  (if(= num 2)
+     (new message% [parent panelx]
+            [label (read-bitmap "EspacioMaquina.png" )])
+     
+     #f
+  )
+     
+)
+  
+
+
+;Crea una fila de la matriz en la interfaz
+(define (crear_fila_mat_widget)
+   (new horizontal-panel%	 
+   	 	[parent panel]	 
+                [style '(border)]
+                [alignment '(left top)]
+    )
+)
+
+;Logica crear fila matriz interfaz
+(define (crear_fila_matriz_interfaz fila widget)
+  (cond
+    ( (null? fila ) 1
+       
+    )(else
+          (crear_espacio (car fila) widget)
+          (crear_fila_matriz_interfaz (cdr fila) widget)
+        )
+  )
+)
+
+;Logica crear matriz interfaz
+(define(crear_matriz_interfaz matriz)
+  (cond
+    ( (null? matriz ) 1)
+      
+    ( (not(list? matriz)) #t) 
+    
+    (else
+          (crear_fila_matriz_interfaz (car matriz) (crear_fila_mat_widget))
+          (crear_matriz_interfaz (cdr matriz))
+        )
+  )
+)
 
 ;Función que crea n botones para las columnas
 (define (crear_botones_columnas num cont)
@@ -95,19 +161,39 @@
                    (crear_botones_columnas (string->number (send columnas_TF get-value)) 0)
                    (write (generateMatrx (string->number (send columnas_TF get-value)) (string->number (send filas_TF get-value))))
                    (asignar_matriz (generateMatrx (string->number (send columnas_TF get-value)) (string->number (send filas_TF get-value))))
+                   (crear_matriz_interfaz (car matrix))
                   )
                   (else (writeln #f))
                 )
                          )]
              )
 
-;Definir el área de juego del 
-(define panel(new horizontal-panel%	 
+;Definir el área de los botones de interfaz
+(define panel_botones(new horizontal-panel%	 
    	 	[parent frame]	 
                 [style '(border)]
                 [alignment '(left top)]
-   	 ))	 
+   	 ))
 
-;
+;Definir el área de juego de interfaz
+(define panel(new vertical-panel%	 
+   	 	[parent frame]	 
+                [style '(border)]
+                [alignment '(left top)]
+   	 ))
+ 
+
+;Reiniciar el tablero d ejuego
+
+(define (refrescar_tablero)
+    (send frame delete-child panel)
+     (set! panel (new vertical-panel%	 
+   	 	[parent frame]	 
+                [style '(border)]
+                [alignment '(left top)]
+   	 )
+
+     )
+)
 
 (send frame show #t)
